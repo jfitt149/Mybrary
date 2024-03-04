713f18344be6ba73ab861834694511887c6ceab3
jfitt149 <jfitt149@gmail.com>
2024-03-04 11:20:03 -0400

    added backend for books and authors

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
@@ -6,5 +7,32 @@ const authorSchema = new mongoose.Schema({
         required: true
     }
 })
+
+authorSchema.pre("deleteOne", async function (next) {
+    try {
+        const query = this.getFilter()
+        const hasBook = await Book.exists({ author: query._id })
+  
+        if (hasBook) {
+            next(new Error("This author still has books."))
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
+// authorSchema.pre('remove', function(next) {
+//     Book.find({ author: this.id }, (err, books) => {
+//         if (err) {
+//             next(err)
+//         } else if (books.length > 0) {
+//             next(new Error('This author still has books'))
+//         } else {
+//             next()
+//         }
+//     })
+// })
 
 module.exports = mongoose.model('Author', authorSchema)
---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,9 @@
+FilePond.registerPlugin(FilePondPluginFileEncode, FilePondPluginImagePreview, FilePondPluginImageResize)
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150/100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150,
+})
+
+FilePond.parse(document.body)
